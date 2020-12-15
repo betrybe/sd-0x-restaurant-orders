ea82d14bc1cb2654d0e78bd871bd919beb30c502
“brunobatista” <brunobatista101@gmail.com>
2020-12-15 10:18:15 -0300

    ajustes de flake e testes

--- tests/test_analyse_log.py
+++ tests/test_analyse_log.py
@@ -1,20 +1,29 @@
 from src.analyse_log import analyse_log
 import pytest
 
+
 def test_validar_conteudo_do_arquivo_gerado():
-    analyse_log(file_content_mock)
+    analyse_log('data/orders_1.csv')
     FILE_TXT = "data/mkt_campaign.txt"
     with open(FILE_TXT) as f:
         file_txt_file = f.readlines()
     assert "hamburguer\n" in file_txt_file
     assert "0\n" in file_txt_file
 
- 
+
 def test_validar_arquivo_inexistente():
-    with pytest.raises(FileNotFoundError, match="No such file or directory: \'data/orders_3.csv\'"):
+    expect_text = (
+           "No such file or directory: "
+           "\'data/orders_3.csv\'"
+         )
+    with pytest.raises(FileNotFoundError, match=expect_text):
         assert analyse_log("data/orders_3.csv")
 
 
 def test_validar_arquivo_com_extensao_invalida():
-    with pytest.raises(FileNotFoundError, match="No such file or directory: \'data/orders_1.txt\'"):
+    expect_text = (
+               "No such file or directory: "
+               "\'data/orders_1.txt\'"
+             )
+    with pytest.raises(FileNotFoundError, match=expect_text):
         assert analyse_log("data/orders_1.txt")
