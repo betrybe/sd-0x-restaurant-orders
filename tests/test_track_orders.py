af74a2f0797dd3c02b25ee14f7218d81e7aad6b2
“brunobatista” <brunobatista101@gmail.com>
2021-01-11 16:54:24 -0300

    ajustes teste trackorders

--- tests/test_track_orders.py
+++ tests/test_track_orders.py
@@ -2,15 +2,15 @@ from src.track_orders import TrackOrders
 
 
 csv_parsed = [
-    ['maria', 'pizza', 'terça-feira'],
-    ['maria', 'hamburguer', 'terça-feira'],
-    ['joao', 'hamburguer', 'terça-feira'],
-    ['maria', 'coxinha', 'segunda-feira'],
-    ['arnaldo', 'misto-quente', 'terça-feira'],
-    ['jose', 'hamburguer', 'sabado'],
-    ['maria', 'hamburguer', 'terça-feira'],
-    ['maria', 'hamburguer', 'terça-feira'],
-    ['joao', 'hamburguer', 'terça-feira']
+    ["maria", "pizza", "terça-feira"],
+    ["maria", "hamburguer", "terça-feira"],
+    ["joao", "hamburguer", "terça-feira"],
+    ["maria", "coxinha", "segunda-feira"],
+    ["arnaldo", "misto-quente", "terça-feira"],
+    ["jose", "hamburguer", "sabado"],
+    ["maria", "hamburguer", "terça-feira"],
+    ["maria", "hamburguer", "terça-feira"],
+    ["joao", "hamburguer", "terça-feira"],
 ]
 
 
@@ -21,7 +21,7 @@ def test_validar_classe_inicia_com_len_0():
 
 def test_validar_adicionar_pedido():
     track_orders = TrackOrders()
-    track_orders.add_new_order('jorge', 'frango', 'domingo')
+    track_orders.add_new_order("jorge", "frango", "domingo")
     assert len(track_orders) == 1
 
 
@@ -29,43 +29,43 @@ def test_validar_prato_mais_pedido():
     track_orders = TrackOrders()
     for name, food, day in csv_parsed:
         track_orders.add_new_order(name, food, day)
-    most_ordered = track_orders.get_most_ordered_dish_per_costumer('maria')
-    assert 'hamburguer' == most_ordered
+    most_ordered = track_orders.get_most_ordered_dish_per_costumer("maria")
+    assert "hamburguer" == most_ordered
 
 
 def test_validar_pedido_nunca_feito_pelo_cliente():
     track_orders = TrackOrders()
     for name, food, day in csv_parsed:
         track_orders.add_new_order(name, food, day)
-    never_ordered = track_orders.get_never_ordered_per_costumer('joao')
-    assert 'coxinha' in never_ordered == {"coxinha", "pizza", "misto-quente"}
+    never_ordered = track_orders.get_never_ordered_per_costumer("joao")
+    assert "coxinha" in never_ordered == {"coxinha", "pizza", "misto-quente"}
 
 
 def test_validar_dia_que_nunca_foi_feito_pedido():
     track_orders = TrackOrders()
     for name, food, day in csv_parsed:
         track_orders.add_new_order(name, food, day)
-    never_visited = track_orders.get_days_never_visited_per_costumer('joao')
-    assert {'segunda-feira', 'sabado'} == never_visited
+    never_visited = track_orders.get_days_never_visited_per_costumer("joao")
+    assert {"segunda-feira", "sabado"} == never_visited
 
 
 def teste_validar_dia_mais_movimentado():
     track_orders = TrackOrders()
-    track_orders.add_new_order('jorge', 'frango', 'domingo')
-    track_orders.add_new_order('jorge', 'frango', 'domingo')
-    track_orders.add_new_order('arnaldo', 'peixe', 'sábado')
-    track_orders.add_new_order('maria', 'carne', 'terça-feira')
-    track_orders.add_new_order('joao', 'salada', 'segunda-feira')
+    track_orders.add_new_order("jorge", "frango", "domingo")
+    track_orders.add_new_order("jorge", "frango", "domingo")
+    track_orders.add_new_order("arnaldo", "peixe", "sábado")
+    track_orders.add_new_order("maria", "carne", "terça-feira")
+    track_orders.add_new_order("joao", "salada", "segunda-feira")
     busiest = track_orders.get_busiest_day()
-    assert 'domingo' == busiest
-    
+    assert "domingo" == busiest
+
+
 def teste_validar_dia_menos_movimentado():
     track_orders = TrackOrders()
-    track_orders.add_new_order('jorge', 'frango', 'domingo')
-    track_orders.add_new_order('jorge', 'frango', 'domingo')
-    track_orders.add_new_order('arnaldo', 'peixe', 'sábado')
-    track_orders.add_new_order('maria', 'carne', 'sábado')
-    track_orders.add_new_order('joao', 'salada', 'segunda-feira')
+    track_orders.add_new_order("jorge", "frango", "domingo")
+    track_orders.add_new_order("jorge", "frango", "domingo")
+    track_orders.add_new_order("arnaldo", "peixe", "sábado")
+    track_orders.add_new_order("maria", "carne", "sábado")
+    track_orders.add_new_order("joao", "salada", "segunda-feira")
     less_busy = track_orders.get_least_busy_day()
-    assert 'segunda-feira' == less_busy
-    
+    assert "segunda-feira" == less_busy
